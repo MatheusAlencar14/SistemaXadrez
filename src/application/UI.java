8cddf7e9c0ad86b76577d6a216cb258206e9941b
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-07-25 15:43:44 -0300

    Implementando troca de jogador a cada turno

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,5 +1,6 @@
 package application;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -45,6 +46,13 @@ public class UI {
             throw new InputMismatchException("Erro ao ler a posição do xadrez. Os valores válidos são" +
                     " de a1 a h8");
         }
+    }
+
+    public static void printMatch(ChessMatch chessMatch) {
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turno: " + chessMatch.getTurn());
+        System.out.println("Aguardando próximo jogador: " + chessMatch.getCurrentPlayer());
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
