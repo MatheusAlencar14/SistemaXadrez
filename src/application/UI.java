6f85f146023920f0ba0a23452a1b402981976d58
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-07-27 15:45:47 -0300

    Lógica de cheque

--- src/application/UI.java
+++ src/application/UI.java
@@ -59,6 +59,9 @@ public class UI {
         System.out.println();
         System.out.println("Turno: " + chessMatch.getTurn());
         System.out.println("Aguardando próximo jogador: " + chessMatch.getCurrentPlayer());
+        if (chessMatch.getCheck()) {
+            System.out.println("CHECK!");
+        }
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
@@ -108,7 +111,7 @@ public class UI {
         System.out.println(Arrays.toString(white.toArray()));
         System.out.print(ANSI_RESET);
         System.out.print("Pretas: ");
-        System.out.print(ANSI_BLACK);
+        System.out.print(ANSI_YELLOW);
         System.out.println(Arrays.toString(black.toArray()));
         System.out.print(ANSI_RESET);
     }
